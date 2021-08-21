d47f444e6c2660edddb849591660fc104d0d47cc
alaswadiyy <abdulmuizsharafadeen@gmail.com>
2021-08-21 01:10:35 +0100

    new commit

---
+++ main.js
@@ -0,0 +1,23 @@
+// Name Type
+let typed = new Typed(".name-typing", {
+    strings: ["Abdulmuiz Sharafadeen"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+});
+
+// Skill Type
+let typedSkill = new Typed(".skill-typing", {
+    strings: ["Jnr Frontend Developer", "Freelancer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+});
+
+// Nick Type
+let typedNick = new Typed(".nick-typing", {
+    strings: ["alaswadiyy"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+});
